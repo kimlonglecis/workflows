329b4649c0b08eef1190df1da41ed8be16767383
Long Le <kimlongyakura@yahoo.com>
2016-01-23 13:35:07 -0800

    Installed gulp all

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 	gutil = require('gulp-util');
 	coffee = require('gulp-coffee');
 	browserify = require('gulp-browserify');
+	compass = require('gulp-compass');
 	concat = require('gulp-concat');
 
 
@@ -10,6 +11,7 @@ var gulp = require('gulp');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 
 
@@ -23,9 +25,23 @@ gulp.task('coffee', function(){
 
 
 gulp.task('js', function() {
-  return gulp.src(jsSources)
+   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
 });
 
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+        css: 'builds/development/css',
+    	sass: 'components/sass',
+    	image: 'builds/development/images',
+    	style: 'expanded',
+        comments: 'true'
+    	}))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('all', ['coffee', 'js', 'compass']);
