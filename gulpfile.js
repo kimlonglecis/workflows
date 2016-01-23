167b05c3f8682079aa2d94462e56e30430b0c01d
Long Le <kimlongyakura@yahoo.com>
2016-01-23 14:26:58 -0800

    Installed gulp-connect, livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 	coffee = require('gulp-coffee');
 	browserify = require('gulp-browserify');
 	compass = require('gulp-compass');
+    connect = require('gulp-connect');
 	concat = require('gulp-concat');
 
 
@@ -28,7 +29,8 @@ gulp.task('js', function() {
    gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'));
+    .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -42,12 +44,20 @@ gulp.task('compass', function() {
     	}))
     .on('error', gutil.log)
     .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
-    gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch('components/sass/*.scss', ['compass'])
     });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('connect', function(){
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+        })
+    });
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
